4a533e2c727bcc0ebff47e49c4c0ccbc4c156eb3
drplote <drplote@gmail.com>
2020-09-03 19:02:46 -0400

    Incremented version #

--- pak-it.bat
+++ pak-it.bat
@@ -3,5 +3,6 @@ mkdir ..\PakTemp
 CScript zip.vbs . ..\PakTemp\2ePlayerOption.zip
 ren ..\PakTemp\2ePlayerOption.zip 2ePlayerOption.ext
 xcopy /s/y ..\PakTemp\2ePlayerOption.ext "C:\Users\drplo\AppData\Roaming\Fantasy Grounds\extensions\"
+xcopy /s/y ..\PakTemp\2ePlayerOption.ext "C:\Users\drplo\AppData\Roaming\SmiteWorks\Fantasy Grounds\extensions\"
 pause
 
