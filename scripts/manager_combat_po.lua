6912db5fb9e4c8cc86f2e898139df70ce5b2002d
drplote <drplote@gmail.com>
2020-10-07 16:14:21 -0400

    Added alternate press behavior for next actor button

--- scripts/manager_combat_po.lua
+++ scripts/manager_combat_po.lua
@@ -192,3 +192,88 @@ function getHpKicker(nodeChar)
     end
     return 20;
 end
+
+function nextActor()
+    if Input.isShiftPressed() then
+        delayThenNextActor(5);
+    elseif Input.isAltPressed() then
+        delayThenNextActor(1);
+    elseif Input.isControlPressed() then
+        delayThenNextActor(10);
+    else
+        CombatManager.nextActor();    
+    end
+end
+
+function delayThenNextActor()
+    if not User.isHost() then
+        return;
+    end
+
+    local nodeActive = CombatManager.getActiveCT();
+    local nIndexActive = 0;
+    
+    -- Check the skip hidden NPC option
+    local bSkipHidden = OptionsManager.isOption("CTSH", "on");
+    
+    -- Determine the next actor
+    local nodeNext = nil;
+    local aEntries = CombatManager.getSortedCombatantList();
+    if #aEntries > 0 then
+        if nodeActive then
+            for i = 1,#aEntries do
+                if aEntries[i] == nodeActive then
+                    nIndexActive = i;
+                    break;
+                end
+            end
+        end
+        if bSkipHidden then
+            local nIndexNext = 0;
+            for i = nIndexActive + 1, #aEntries do
+                if DB.getValue(aEntries[i], "friendfoe", "") == "friend" then
+                    nIndexNext = i;
+                    break;
+                else
+                    if not CombatManager.isCTHidden(aEntries[i]) then
+                        nIndexNext = i;
+                        break;
+                    end
+                end
+            end
+            if nIndexNext > nIndexActive then
+                nodeNext = aEntries[nIndexNext];
+                for i = nIndexActive + 1, nIndexNext - 1 do
+                    CombatManager.showTurnMessage(aEntries[i], false);
+                end
+            end
+        else
+            nodeNext = aEntries[nIndexActive + 1];
+        end
+        
+    end
+
+    local nInitDelay = 10;
+
+    -- If next actor available, advance effects, activate and start turn
+    if nodeNext then
+        local nPreviousInit = DB.getValue(nodeActive, "initresult");
+        local nNextActorInit = DB.getValue(nodeNext, "initresult");
+        if (nPreviousInit + nInitDelay < nNextActorInit) then -- TODO: This might need to be <=
+            -- End turn for current actor
+            CombatManager.onTurnEndEvent(nodeActive);
+        
+            -- Process effects in between current and next actors
+            if nodeActive then
+                CombatManager.onInitChangeEvent(nodeActive, nodeNext);
+            else
+                CombatManager.onInitChangeEvent(nil, nodeNext);
+            end
+            
+            -- Start turn for next actor
+            CombatManager.requestActivation(nodeNext, bSkipBell);
+            CombatManager.onTurnStartEvent(nodeNext);
+        end
+    end
+    DB.setValue(nodeActive, "initresult", "number", nPreviousInit + nInitDelay);
+end
