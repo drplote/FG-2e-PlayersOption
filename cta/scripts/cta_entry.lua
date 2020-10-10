dd787ed5536cfa6974b3b30a121db9d10d4bba2f
drplote <drplote@gmail.com>
2020-10-09 22:36:33 -0400

    Changed UI on host CTA to show phase names instead of numbers

---
+++ cta/scripts/cta_entry.lua
@@ -0,0 +1,33 @@
+function onInit()
+    super.onInit();
+    local node = getDatabaseNode();
+    OptionsManager.registerCallback(PlayerOptionManager.sPhasedInitiativeOptionKey, update);   
+    DB.addHandler(DB.getPath(node, "initresult"), "onUpdate", updateInitResult);
+    update();
+end
+
+function onClose()
+    super.onClose();
+    local node = getDatabaseNode();
+    DB.removeHandler(DB.getPath(node, "initresult"), "onUpdate", updateInitResult);
+end
+
+function updateInitResult()
+    if PlayerOptionManager.isUsingPhasedInitiative() then
+        local node = getDatabaseNode();
+        local nInitResult = DB.getValue(node, "initresult", 12);
+        local sPhaseName = InitManagerPO.getPhaseName(math.floor(nInitResult/2));
+        DB.setValue(node, "initresultpo", "string", sPhaseName);
+        initresultpo.setValue(sPhaseName);
+    end
+end
+
+function update()
+    if PlayerOptionManager.isUsingPhasedInitiative() then
+        initresult.setVisible(false);
+        initresultpo.setVisible(true);
+    else
+        initresult.setVisible(true);
+        initresultpo.setVisible(false);
+    end
+end
