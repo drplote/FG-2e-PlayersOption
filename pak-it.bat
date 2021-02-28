0cfc9c5cb5e4b841f167156de263e34a2e34ac26
drplote <drplote@gmail.com>
2021-02-28 14:05:33 -0500

    Update pak-it.bat

--- pak-it.bat
+++ pak-it.bat
@@ -2,6 +2,6 @@ rmdir /s/q ..\PakTemp
 mkdir ..\PakTemp
 CScript zip.vbs . ..\PakTemp\2ePlayerOption.zip
 ren ..\PakTemp\2ePlayerOption.zip 2ePlayerOption.ext
-xcopy /s/y ..\PakTemp\2ePlayerOption.ext "C:\Users\drplote\SmiteWorks\Fantasy Grounds\extensions"
+xcopy /s/y ..\PakTemp\2ePlayerOption.ext "C:\Users\drplo\SmiteWorks\Fantasy Grounds\extensions"
 pause
 
